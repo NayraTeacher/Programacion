2091dae4428e63b6ec37c01f9aa80bc685cd19e5
Nayra <Nayra@DESKTOP-41P84Q5.localdomain>
2020-10-21 09:05:16 +0200

    Primera version 1.0

---
+++ holaclase/src/holaclase/holaclase.java
@@ -0,0 +1,12 @@
+package holaclase;
+
+public class holaclase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola clase de primero de informatica otra vez");
+		
+	}
+
+}
