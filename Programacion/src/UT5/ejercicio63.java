14c6d9ec90a8d82a7e60b494c4cd15bd1b294497
Nayra <Nayra@DESKTOP-41P84Q5.localdomain>
2020-11-16 13:46:36 +0100

    clase 16/11

---
+++ Programacion/src/UT5/ejercicio63.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package UT5;
+
+/**
+ * @author Nayra
+ * Crear un array de enteros de 10 posiciones, pedir números al usuario hasta rellenarlo. 
+ * Mostrar a continuación la media de los numeros del array y cuál es el mayor y el menor. 
+ */
+public class ejercicio63 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
