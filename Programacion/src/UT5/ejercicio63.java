afbad5c2d7d4bc0ce64da9419fb5cbb969ef5d86
Nayra <Nayra@DESKTOP-41P84Q5.localdomain>
2020-11-17 12:18:04 +0100

    ej 63 completado

--- Programacion/src/UT5/ejercicio63.java
+++ Programacion/src/UT5/ejercicio63.java
@@ -3,6 +3,8 @@
  */
 package UT5;
 
+import java.util.Scanner;
+
 /**
  * @author Nayra
  * Crear un array de enteros de 10 posiciones, pedir números al usuario hasta rellenarlo. 
@@ -14,7 +16,36 @@ public class ejercicio63 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in);
+		int enteros[];
+		enteros = new int[10];
+		int suma_de_numeros=0;
+		int menor = 0;
+        int mayor = 0;
+		
+		//Bucle para recorrer array y completarlo con valores introducidos por el usuario
+		for(int i=0; i<enteros.length; i++) {
+			System.out.println("Introduce el numero de la posicion "+i);
+			enteros[i]=entrada.nextInt();
+		}
+		
+		for(int j=0; j<enteros.length; j++) {
+			suma_de_numeros = suma_de_numeros + enteros[j];
+			if (j == 0) {
+	      		menor=enteros[j];
+	      		mayor=enteros[j];
+	      	}
+	      	
+	      	if (enteros[j]<menor)
+	      		menor=enteros[j];
+	      	if (enteros[j]>mayor)
+	      		mayor=enteros[j];	 
+			
+		}
+		
+		System.out.println("La media de los numeros es " + suma_de_numeros/10);
+		System.out.printf("Este es el numero menor: %d\n", menor);
+	    System.out.printf("Este es el numero mayor: %d\n", mayor);
 
 	}
 
