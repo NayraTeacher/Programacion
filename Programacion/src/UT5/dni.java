60cf91883ab539f40512e1a8206d246e733634f9
Nayra Deniz <nayradb@gmail.com>
2021-01-11 11:40:48 +0100

    Version ejercicios 74 y dni usando funciones

--- Programacion/src/UT5/dni.java
+++ Programacion/src/UT5/dni.java
@@ -15,29 +15,24 @@ public class dni {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in); 
-
-        
+        Scanner sc=new Scanner(System.in);      
 
         int dni; 
-
-        char letra[]= {'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'}; 
-
-        int resto; 
-
         System.out.println("Escribe tu numero del DNI y te digo la letra que tiene"); 
-
-        dni=sc.nextInt(); 
-
-     
-        resto=dni%23;        
-
-        System.out.println("La letra de tu DNI es " + letra[resto]); 
-
-         
-
+        dni=sc.nextInt();       
+        System.out.println("La letra de tu DNI es " + letraDNI(dni)); 
         sc.close(); 
 
+	}
+	
+	
+	private static char letraDNI(int dni) {
+		char resultado;
+        char letra[]= {'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'}; 
+        int resto; 
+        resto=dni%23;        
+		resultado=letra[resto];
+		return resultado;
 	}
 
 }
--- Programacion/src/UT5/ejercicio74.java
+++ Programacion/src/UT5/ejercicio74.java
@@ -31,47 +31,75 @@ public class ejercicio74 {
 		int numero[]=new int[50]; 
         int matriz[][]; 
         int divisor; 
-        int resultado;
-        int cont=0; 
 
-        for(int i=0;i<numero.length;i++) { 
-            numero[i]=(int)(Math.random()*100)+1;  
-        } 
 
-        for(int i=0;i<numero.length;i++) { 
-            System.out.print(numero[i]+" "); 
-        } 
-
-//Filas=divisor y columnas=resultado 
-
+        //Funciones, aportan:
+        //- Mejor legibilidad del código
+        //- Reutilización de código
+        //- Facilidad de depuración de errores
+        //- Centralización de código
+        
+        fillrandom(numero);
+        pintaArray1(numero);
+        
         System.out.println("Numero por el que quieres dividir el Array (divisor de 50): "); 
-        divisor=entrada.nextInt(); 
-        resultado=50/divisor; 
+        divisor=entrada.nextInt();         
+        matriz=subdividirArray1(numero,divisor);
 
-        matriz=new int[divisor][resultado]; 
+        pintaArray2(matriz);
+        pintaArray1(numero);
+
+    } 
+	
+	//funcion que rellene array con numeros aleatorios, le paso el array
+	private static void fillrandom(int[] array) {
+        for(int i=0;i<array.length;i++) { 
+            array[i]=(int)(Math.random()*100)+1;  
+        } 
+	}
+	
+	//Funcion que me pinta un array unidimensional
+	private static void pintaArray1(int[] array) {
+		for(int i=0;i<array.length;i++) { 
+            System.out.print(array[i]+" "); 
+        } 
+		System.out.print("\n"); 
+	}
+	
+	//Funcion que me pinta un array bidimensional
+	private static void pintaArray2(int[][] array) {
+		for(int i=0;i<array.length;i++) { 
+            System.out.print("\n"); 
+            for(int j=0;j<array[i].length;j++) { 
+                System.out.print(array[i][j]+" "); 
+            } 
+            
+        }  
+		System.out.print("\n"); 
+	}
+	
+	//funcion que subidivide un array unidimensional que le paso, y devuelve una matriz con las divisiones
+	private static int[][] subdividirArray1(int[] array, int divisiones){
+		int matriz[][]; 
+		int resultado=array.length/divisiones; 
+        int cont=0; 
+        
+        matriz=new int[divisiones][resultado]; 
 
 //2 1 5 6 3 4 9 8 7 ej array de 9 subdividido en 3
 //2 1 5 
 //6 3 4 
 //9 8 7 
 
-        for(int i=0;i<divisor;i++) { 
+        for(int i=0;i<divisiones;i++) { 
             for(int j=0;j<resultado;j++) {
-                matriz[i][j]=numero[cont]; 
+                matriz[i][j]=array[cont]; 
                 cont++; 
             } 
         } 
-
-//Pintar la matriz. 
-
-        for(int i=0;i<matriz.length;i++) { 
-            System.out.print("\n"); 
-            for(int j=0;j<matriz[i].length;j++) { 
-                System.out.print(matriz[i][j]+" "); 
-            } 
-        } 
-
-    } 
-	
+		
+		return matriz;
+		
+	}
 
 }
