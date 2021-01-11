1abb4d28727f470a22d0387e634503c4a235ede4
Nayra Deniz <nayradb@gmail.com>
2021-01-11 12:16:41 +0100

    version con funciones

--- Programacion/src/UT5/ejercicio71.java
+++ Programacion/src/UT5/ejercicio71.java
@@ -14,33 +14,18 @@ public class ejercicio71 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-        
-
         // TODO Auto-generated method stub 
         int [][] a = new int[3][3]; 
         int [][] b = new int[3][3]; 
         int [][] c = new int[3][3]; 
 
         //Cargar tabla a con numeros aleatorios             
-            for (int i=0; i<a.length; i++) {                 
-                for (int j=0; j<a[i].length; j++) {                   
-                    a[i][j]=(int)(Math.random()*10); 
-                } 
-            } 
-
-            //Cargar tabla b con numeros aleatorios          
-            for (int i=0; i<b.length; i++) { 
-                for (int j=0; j<b[i].length; j++) {                       
-                    b[i][j]=(int)(Math.random()*10); 
-                } 
-            } 
+            fillRandomTable(a);
+        //Cargar tabla b con numeros aleatorios          
+            fillRandomTable(b);
 
             //Hacer suma (que se mete en tabla c) 
-            for (int i=0; i<c.length; i++) { 
-                for (int j=0; j<c[i].length; j++) { 
-                    c[i][j] =  a[i][j] + b[i][j];                   
-                } 
-            } 
+            c=sumaMatrices(a,b);
 
             //pintar tabla a  
             System.out.println("\nEste es el primer array"); 
@@ -75,6 +60,27 @@ public class ejercicio71 {
 
             } 
 
+	}
+	
+	//funcion que carga una tabla (matriz o array bidimensional) con numeros aleatorios
+	private static void fillRandomTable(int[][] tabla) {
+		for (int i=0; i<tabla.length; i++) {                 
+            for (int j=0; j<tabla[i].length; j++) {                   
+            	tabla[i][j]=(int)(Math.random()*10); 
+            } 
+        } 		
+	}
+	
+	//funcion suma de matrices, le paso dos matrices y me devuelve una tercera con la suma
+	private static int[][] sumaMatrices(int[][] matriz1, int[][] matriz2){
+		int[][] suma = new int[matriz1.length][matriz1.length];;
+		for (int i=0; i<suma.length; i++) { 
+            for (int j=0; j<suma[i].length; j++) { 
+                suma[i][j] =  matriz1[i][j] + matriz2[i][j];                   
+            } 
+        } 
+		
+		return suma;
 	}
 
 }
